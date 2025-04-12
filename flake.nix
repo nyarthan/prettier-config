64f470a74270158b4da0519783a78e68a834deae
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2025-04-12 19:52:05 +0200

    chore: initialize repo

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Formeta flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_22
+	      pkgs.pnpm_10
+            ];
+          };
+        };
+    };
+}
