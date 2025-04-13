965fa330b4c4612ad458270415ecd0583acffcc9
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2025-04-13 15:03:05 +0200

    chore: format

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,6 @@
-import { RuleConfigSeverity, type UserConfig } from "@commitlint/types";
+import type { UserConfig } from "@commitlint/types";
+
+import { RuleConfigSeverity } from "@commitlint/types";
 
 const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
