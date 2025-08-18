c73623f18d1c6fde88eaa700c79258a3758873c2
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2025-08-18 21:24:09 +0200

    fix: type exports

--- rslib.config.ts
+++ rslib.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "@rslib/core";
 
 export default defineConfig({
   source: { entry: { index: "./src/index.ts" } },
-  lib: [
-    { format: "esm", syntax: "es2021", dts: true },
-    { format: "cjs", syntax: "es2021", dts: true },
-  ],
+  lib: (["esm", "cjs"] as const).map((format) => ({
+    format,
+    syntax: "es2021",
+    dts: { autoExtension: true },
+  })),
   output: {
     target: "node",
   },
